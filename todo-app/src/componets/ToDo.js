d0b56aa760f08c38f550095bbcc93cf4c031d44e
Awais Ahmad <awais6412l@gmail.com>
2022-11-18 09:00:00 +0500

    delete functionality added

--- todo-app/src/componets/ToDo.js
+++ todo-app/src/componets/ToDo.js
@@ -7,15 +7,23 @@ import ShowData from './ShowData';
 function ToDo() {
   const [task, setTask] = useState('add Task');
   const [data, setData] = useState([]);
-  const onChangeHandler = e => {
+  const onChangeHandler = (e) => {
     setTask(e.target.value);
-  }
+  };
   const submitHandler = (e) => {
     e.preventDefault();
     const newData = task;
     setData([...data, newData]);
     setTask('');
-  }
+  };
+  const deleteItem = (deleteId) => {
+    console.log('delete');
+    let finalData = data.filter((currentItem, index)=>{
+       return index !== deleteId;
+    })
+    setData(finalData);
+
+  };
   return (
     <div className="bg-lightblue">
       <div className="container">
@@ -23,25 +31,27 @@ function ToDo() {
           <div className="col-md-4 mx-auto">
             <form className="pt-5 pb-2" onSubmit={submitHandler}>
               <div className="d-flex">
-                <input className="form-control rounded-0 mr__2" value={task} onChange={onChangeHandler}></input>
-                <Button type='submit' variant="primary" className="btn__bg">
+                <input
+                  className="form-control rounded-0 mr__2"
+                  value={task}
+                  onChange={onChangeHandler}
+                ></input>
+                <Button type="submit" variant="primary" className="btn__bg">
                   add
                 </Button>
               </div>
             </form>
             <div>
-              {
-                data.map((value, index)=>{
-                 return <ShowData
-                   key={index}
-                   id={index}
-                   task={value}
-              />
-                })
-              }
-              
-              {console.log(data)}
-              
+              {data.map((value, index) => {
+                return (
+                  <ShowData
+                    key={index}
+                    id={index}
+                    task={value}
+                    onSelect={deleteItem}
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
