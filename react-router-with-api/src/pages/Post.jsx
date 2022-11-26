f93e9053225a0fcbd79fdb4a8c0c9147ed35f8b6
Awais Ahmad <awais6412l@gmail.com>
2022-11-26 22:13:38 +0500

    dynamic routing with params added

--- react-router-with-api/src/pages/Post.jsx
+++ react-router-with-api/src/pages/Post.jsx
@@ -3,21 +3,19 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function Post() {
-  const [postData, setPost] = useState({id:{}});
+  const [postData, setPost] = useState({});
   const { id } = useParams();
   function getPost() {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((result) => result.json())
       .then((data) => {
         setPost(data);
+        console.log(postData)
       });
   }
   useEffect(() => {
     getPost();
   }, []);
-  return <div>{postData.map(element=>(
-    <h2>{element.id}</h2>
-  ))}</div>
+  return <div>{postData}</div>
 }
-
 export default Post;
