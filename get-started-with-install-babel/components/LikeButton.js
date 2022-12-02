8d137785689efac2da98c28abbb52bc362a59840
Awais Ahmad <awais6412l@gmail.com>
2022-12-02 15:56:00 +0500

    install babel to add jsx

---
+++ get-started-with-install-babel/components/LikeButton.js
@@ -0,0 +1,9 @@
+class LikeButton extends React.Component {
+  render() {
+    return <h1>Hello Programmers!</h1>;
+  }
+}
+ReactDOM.render(<LikeButton />, document.getElementById('root'));
+// const domContainer = document.querySelector('#like_button_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
