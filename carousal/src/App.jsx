ab4f93d2e03308f184706f35befd1e3473a2fd18
Awais Ahmad <awais6412l@gmail.com>
2023-01-17 23:32:24 +0500

    add carousal in react setup

---
+++ carousal/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './assets/style.css';
+function App() {
+  const [current, setCurrent] = useState(0);
+  const images = [
+    'https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    'https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    'https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    'https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  ];
+  const prevSlide = () => {};
+  const nextSlide = () => {};
+  return (
+    <>
+      <div className="slider">
+        <div className="left-arrow" onClick={prevSlide}>
+          ⬅
+        </div>
+        <div className="right-arrow" onClick={nextSlide}>
+          ⮕
+        </div>
+        <div className='slide'>
+          <img src={images[0]} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
