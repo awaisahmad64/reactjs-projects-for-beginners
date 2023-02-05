7ca334d99a2a33441a941112b2e4ff035835618b
Awais Ahmad <awais6412l@gmail.com>
2023-02-05 22:20:32 +0500

    create DigitButton component

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useReducer } from 'react';
 import './App.css';
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  EVALUATE: 'evaluate',
+};
+const reducer = (state, { type, payload }) => {
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,{
+          currentOperand:`${currentOperand}${payload.digit}`}
+        }
+      }
+  }
+}
 function App() {
+  const [currentOperand, previousOperand, operation, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
     <div className="calculator-grid">
       <div className="calculator-output">
-        <div className="previous-oprand">12,3 *</div>
-        <div className="current-oprand">1234</div>
+        <div className="previous-oprand">
+          {previousOperand}
+          {operations}
+        </div>
+        <div className="current-oprand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
---
+++ react-calculator/src/DigitButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DigitButton({dispatch,digit}) {
+  return <button onClick={dispatch(type: )}>{digit}</button>;
+}
+
+export default DigitButton;
